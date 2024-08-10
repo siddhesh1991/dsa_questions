6110f5c525f7d51afdc88a868753b9e81a6547a3
Siddhesh Tiwari <siddhesh.tiwari@gmail.com>
2024-08-10 14:30:07 -0500

    new

--- strings/Palindrome.py
+++ strings/Palindrome.py
@@ -20,3 +20,29 @@ def isPalindrome(string):
     return stringArr == stringArrReverse
 
 
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        j = len(s)-1
+
+        while i < j:
+
+            while i < len(s) and not s[i].isalnum() :
+                i += 1
+
+            while j >= 0 and not s[j].isalnum():
+                j -= 1
+
+            if i <= j :
+                if s[i].lower() == s[j].lower():
+                    i += 1
+                    j -= 1
+                else:
+                    return False
+
+        return True
+
+            
+
+
+
